2d0403ea29183331578891d5153d0b25088aaa32
Ajaybala95 <50860506+Ajaybala95@users.noreply.github.com>
2019-09-20 23:32:01 +0530

    Update pushcrew-sw.js

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrew.com/sw/6523fa920319409af83e5907ca4d582c.js');
+importScripts('https://cdn.pushcrew.com/sw/9507d64086ecb727f7a7940126b8b7cc.js');
